fc8055a0888008082230cd8ed9b100bdf94144b9
DavidRaschidLabiakhTrue <105135091+DavidRaschidLabiakhTrue@users.noreply.github.com>
2023-01-26 18:30:33 -0500

    Add files via upload

--- 2021 Fall/CAP4453 - Robot Vision/vdetect/stdafx.cpp
+++ 2021 Fall/CAP4453 - Robot Vision/vdetect/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// vdetect.pch will be the pre-compiled header
+// vboost.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
